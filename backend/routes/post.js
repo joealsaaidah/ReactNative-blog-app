204c34c8bb3dab742a5f6052a8bfbf60f4097891
joealsaaidah <alsaaidahyousef@gmail.com>
2022-01-28 17:21:17 +0800

    apply validator middleware

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { createPost, latestPost } from "../controllers/post.js";
 import multer from "../middlewares/multer.js";
+import { postValidator, validate } from "../middlewares/postValidator.js";
 
 const router = express.Router();
 
-router.route("/create").post(multer.single("thumbnail"), createPost);
+router
+  .route("/create")
+  .post(multer.single("thumbnail"), postValidator, validate, createPost);
 router.route("/latest").get(latestPost);
 
 export default router;
