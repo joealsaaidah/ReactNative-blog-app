4ad423382a1314f7b0d3c42f7878e77029ddb6c6
joealsaaidah <alsaaidahyousef@gmail.com>
2022-02-01 19:51:06 +0800

    handling form data states

--- admin/src/components/CreatePost.jsx
+++ admin/src/components/CreatePost.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   ImEye,
   ImFilePicture,
   ImFilesEmpty,
   ImSpinner11,
+  ImSpinner3,
 } from "react-icons/im";
+import { uploadImage } from "../api/post";
 
 const mdRules = [
   {
@@ -25,7 +27,76 @@ const mdRules = [
   },
 ];
 
+const defaultPost = {
+  title: "",
+  thumbnail: "",
+  featured: false,
+  content: "",
+  tags: "",
+  meta: "",
+};
+
 const CreatePost = () => {
+  const [postInfo, setPostInfo] = useState({ ...defaultPost });
+  const [selectedThumbnailURL, setSelectedThumbnailURL] = useState("");
+  const [imageUrlToCopy, setImageUrlToCopy] = useState("");
+  const [imageUploading, setImageUploading] = useState(false);
+
+  const { title, featured, content, tags, meta } = postInfo;
+
+  const handleChange = ({ target }) => {
+    const { value, name, checked } = target;
+
+    //thumbnail clicked
+    if (name === "thumbnail") {
+      const file = target.files[0];
+      if (!file.type?.includes("image")) {
+        return alert("this is not an image!");
+      }
+      setPostInfo({ ...postInfo, thumbnail: file });
+      return setSelectedThumbnailURL(URL.createObjectURL(file));
+    }
+    //featured clicked
+    if (name === "featured") {
+      return setPostInfo({ ...postInfo, [name]: checked });
+    }
+    //tags
+    if (name === "tags") {
+      const newTags = tags.split(",");
+      if (newTags.length > 4)
+        console.log("Only the first 4 tags will be selected");
+    }
+    //meta
+    if (name === "meta" && meta.length >= 150) {
+      return setPostInfo({ ...postInfo, [meta]: value.substring(0, 149) });
+    }
+
+    setPostInfo({ ...postInfo, [name]: value });
+  };
+
+  const imageUploadHandler = async ({ target }) => {
+    if (imageUploading) return;
+
+    const file = target.files[0];
+    if (!file.type?.includes("image")) {
+      return alert("this is not an image!");
+    }
+
+    setImageUploading(true);
+    const formData = new FormData();
+    formData.append("image", file);
+    const { error, image } = await uploadImage(formData);
+    setImageUploading(false);
+
+    if (error) return console.log(error);
+    setImageUrlToCopy(image);
+  };
+
+  const onCopyHandler = () => {
+    const textToCopy = `![Add image description] (${imageUrlToCopy})`;
+    navigator.clipboard.writeText(textToCopy);
+  };
+
   return (
     <form className='flex p-2'>
       {/* Left Side */}
@@ -36,11 +107,17 @@ const CreatePost = () => {
             Create New Post
           </h1>
           <div className='flex items-center space-x-5'>
-            <button className='flex items-center h-10 px-3 space-x-2 text-blue-500 transition rounded hover:text-white hover:bg-blue-500 ring-1 ring-blue-500'>
+            <button
+              type='button'
+              className='flex items-center h-10 px-3 space-x-2 text-blue-500 transition rounded hover:text-white hover:bg-blue-500 ring-1 ring-blue-500'
+            >
               <ImSpinner11 />
               <span>Reset</span>
             </button>
-            <button className='flex items-center h-10 px-3 space-x-2 text-blue-500 transition rounded hover:text-white hover:bg-blue-500 ring-1 ring-blue-500'>
+            <button
+              type='button'
+              className='flex items-center h-10 px-3 space-x-2 text-blue-500 transition rounded hover:text-white hover:bg-blue-500 ring-1 ring-blue-500'
+            >
               <ImEye />
               <span>View</span>
             </button>
@@ -50,73 +127,114 @@ const CreatePost = () => {
           </div>
         </div>
         {/* Featured Checkbox */}
-        <div>
-          <input id='featured' type='checkbox' hidden />
+        <div className='flex'>
+          <input
+            name='featured'
+            onChange={handleChange}
+            id='featured'
+            type='checkbox'
+            hidden
+          />
           <label
-            className='flex items-center space-x-2 text-gray-700 cursor-pointer group'
+            className='flex items-center space-x-2 text-gray-700 cursor-pointer select-none group'
             htmlFor='featured'
           >
             <div className='flex items-center justify-center w-4 h-4 border-2 border-gray-700 rounded-full group-hover:border-blue-500'>
-              <div className='w-2 h-2 bg-gray-700 rounded-full group-hover:bg-blue-500 ' />
+              {featured && (
+                <div className='w-2 h-2 bg-gray-700 rounded-full group-hover:bg-blue-500 ' />
+              )}
             </div>
             <span className='group-hover:text-blue-500'>Featrued</span>
           </label>
         </div>
         {/* Title input */}
         <input
+          value={title}
+          name='title'
           className='w-full p-2 text-xl font-semibold rounded outline-none focus:ring-1 '
           placeholder='Post title'
           type='text'
+          onChange={handleChange}
         />
         {/* image input  */}
         <div className='flex space-x-2'>
           <div>
-            <input id='image-input' type='file' hidden />
+            <input
+              onChange={imageUploadHandler}
+              id='image-input'
+              type='file'
+              hidden
+            />
             <label
               htmlFor='image-input'
               className='flex items-center h-10 px-3 space-x-2 text-gray-700 transition rounded cursor-pointer hover:text-white hover:bg-gray-700 ring-1 ring-gray-700'
             >
               <span>PLace image</span>
-              <ImFilePicture />
+              {!imageUploading ? (
+                <ImFilePicture />
+              ) : (
+                <ImSpinner3 className='animate-spin' />
+              )}
             </label>
           </div>
 
-          <div className='flex items-center justify-between flex-1 overflow-hidden bg-gray-400 rounded'>
-            <input
-              className='w-full px-2 text-white bg-transparent'
-              type='text'
-              value='my name is yousef i am 32 years old, living in malaysia'
-              disabled
-            />
+          {imageUrlToCopy && (
+            <div className='flex items-center justify-between flex-1 overflow-hidden bg-gray-400 rounded'>
+              <input
+                className='w-full px-2 text-white bg-transparent'
+                type='text'
+                value={imageUrlToCopy}
+                disabled
+              />
 
-            <button className='flex flex-col items-center self-stretch justify-center p-1 text-xs text-white bg-gray-700'>
-              <ImFilesEmpty />
-              <span>copy</span>
-            </button>
-          </div>
+              <button
+                onClick={onCopyHandler}
+                type='button'
+                className='flex flex-col items-center self-stretch justify-center p-1 text-xs text-white bg-gray-700'
+              >
+                <ImFilesEmpty />
+                <span>copy</span>
+              </button>
+            </div>
+          )}
         </div>
+
+        {/* content  */}
         <textarea
+          value={content}
+          name='content'
           className='flex-1 w-full p-2 font-mono text-lg tracking-wide rounded outline-none resize-none focus:ring-1'
           placeholder='## Markdown'
+          onChange={handleChange}
         ></textarea>
 
         {/* Tags input */}
         <div>
-          <label htmlFor='tags'>Tags</label>
+          <label className='text-gray-500' htmlFor='tags'>
+            Tags
+          </label>
           <input
+            value={tags}
+            name='tags'
             id='tags'
             className='w-full p-2 rounded outline-none focus:ring-1 '
-            placeholder='Post title'
+            placeholder='tag1, tag2, tag3'
             type='text'
+            onChange={handleChange}
           />
         </div>
         {/* meta description input */}
         <div>
-          <label htmlFor='meta'>Meta description</label>
+          <label className='text-gray-500' htmlFor='meta'>
+            Meta description {meta.length} / 150
+          </label>
           <textarea
+            value={meta}
+            name='meta'
             id='meta'
             className='w-full p-2 rounded outline-none resize-none h-28 focus:ring-1'
             placeholder='Meta description'
+            onChange={handleChange}
           ></textarea>
         </div>
       </div>
@@ -125,13 +243,27 @@ const CreatePost = () => {
         <h1 className='mb-2 text-xl font-semibold text-gray-700'>Thumbnail</h1>
         {/* Image Input */}
         <div>
-          <input id='thumbnail' type='file' hidden />
+          <input
+            name='thumbnail'
+            id='thumbnail'
+            type='file'
+            onChange={handleChange}
+            hidden
+          />
           <label className='cursor-pointer' htmlFor='thumbnail'>
-            <div className='flex flex-col items-center justify-center text-gray-500 border border-gray-500 border-dashed aspect-video'>
-              <span>Select thumbnail</span>
-              <span className='text-xs'>Recommended size</span>
-              <span className='text-xs'>1280 * 720</span>
-            </div>
+            {selectedThumbnailURL ? (
+              <img
+                src={selectedThumbnailURL}
+                className='rounded shadow-sm aspect-video'
+                alt='selected thumbnail'
+              />
+            ) : (
+              <div className='flex flex-col items-center justify-center text-gray-500 border border-gray-500 border-dashed aspect-video'>
+                <span>Select thumbnail</span>
+                <span className='text-xs'>Recommended size</span>
+                <span className='text-xs'>1280 * 720</span>
+              </div>
+            )}
           </label>
         </div>
         {/* Markdown cheat sheet */}
